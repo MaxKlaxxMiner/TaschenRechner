9d0f658f3b9b5fdc97226ca6110fc136a94ea21c
MaxKlaxxMiner <max@tetrisk.de>
2017-09-11 17:39:50 +0200

    FullTest

---
+++ TaschenRechnerLib/UIntLimbs/ToString.cs
@@ -0,0 +1,92 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TaschenRechnerLib
+{
+  public sealed partial class UIntLimbs
+  {
+    /// <summary>
+    /// merkt sich die Char-Map für die Zahlen von 0 bis 999 ('0','0','0', '0','0','1', '0','0','2', '0','0','3', '0','0','4', usw. bis '9', '9', '9')
+    /// </summary>
+    internal static readonly unsafe char* DirectChars1K = GenDirectChars1K();
+
+    /// <summary>
+    /// generiert die Char-Map für die Zahlen von 0 bis 999
+    /// </summary>
+    /// <returns>Zeiger auf die fertige Char-Map</returns>
+    static unsafe char* GenDirectChars1K()
+    {
+      var result = Marshal.AllocHGlobal(1000 * 3 * 2);
+
+      var chars = (char*)result;
+      for (char n0 = '0'; n0 <= '9'; n0++)
+      {
+        for (char n1 = '0'; n1 <= '9'; n1++)
+        {
+          for (char n2 = '0'; n2 <= '9'; n2++)
+          {
+            *chars++ = n0;
+            *chars++ = n1;
+            *chars++ = n2;
+          }
+        }
+      }
+
+      // --- check values ---
+      // var dummy = new char[1000 * 3];
+      // Marshal.Copy(result, dummy, 0, dummy.Length);
+
+      return (char*)result;
+    }
+
+    /// <summary>
+    /// gibt Anzahl der benötigten Ziffern eines Limbs zurück (1 - 9)
+    /// </summary>
+    /// <param name="limb">Limb, welches berechnet werden soll</param>
+    /// <returns>Anzahl der Ziffer, welche benötigt werden</returns>
+    static int GetStrLen(int limb)
+    {
+      if (limb < 100) return limb < 10 ? 1 : 2; // 1-2 Ziffern
+      // --- 3-9 Ziffern ---
+      if (limb < 10000) return limb < 1000 ? 3 : 4; // 3-4 Ziffern
+      // --- 5-9 Ziffern ---
+      if (limb < 1000000) return limb < 100000 ? 5 : 6; // 5-6 Ziffern
+      // --- 7-9 Ziffern ---
+      if (limb < 10000000) return 7; // 7 Ziffern
+      return limb < 100000000 ? 8 : 9; // 8-9 Ziffern
+    }
+
+    /// <summary>
+    /// berechnet die Anzahl der benötigten Ziffern um eine Limbs-Zahl darzustellen
+    /// </summary>
+    /// <param name="limbs">Limbs, welche berechnet werden sollen</param>
+    /// <returns>Anzahl der Ziffer, welche benötigt werden</returns>
+    static int GetStrLen(int[] limbs)
+    {
+      if (limbs.Length == 1) return GetStrLen(limbs[0]);
+
+      return (limbs.Length - 1) * 9 + GetStrLen(limbs[limbs.Length - 1]);
+    }
+
+    /// <summary>
+    /// gibt die Zahl als lesbare Zeichenkette zurück
+    /// </summary>
+    /// <returns>lesbare Zeichenkette</returns>
+    public unsafe override string ToString()
+    {
+      int len = GetStrLen(limbs);
+      string tmp = UnsafeHelper.FastAllocateString(len);
+
+      return "";
+    }
+
+    /// <summary>
+    /// gibt die Zahl als lesbare Zeichenkette mit Tausender-Punkten zurück
+    /// </summary>
+    /// <param name="sepChar">Zeichen, welches für den Tausenderpunkt verwendet werden soll (default: ',')</param>
+    /// <returns>lesbare Zeichenkette</returns>
+    public unsafe string ToStringThousandsSep(char sepChar = ',')
+    {
+      return "";
+    }
+  }
+}
--- TaschenRechnerTest/Program.cs
+++ TaschenRechnerTest/Program.cs
@@ -3,8 +3,8 @@
 // ReSharper disable UnusedVariable
 // ReSharper disable UnusedMember.Local
 
-//using ui = TaschenRechnerLib.UIntSimple;
-using ui = TaschenRechnerLib.UIntLimbs;
+using ui = TaschenRechnerLib.UIntSimple;
+//using ui = TaschenRechnerLib.UIntLimbs;
 
 namespace TaschenRechnerTest
 {
@@ -13,63 +13,105 @@ namespace TaschenRechnerTest
     static void Constructor()
     {
       var val01 = new ui(0);
+      Debug.Assert(val01.ToString() == "0");
       var val02 = new ui(1);
+      Debug.Assert(val02.ToString() == "1");
       var val03 = new ui(9);
+      Debug.Assert(val03.ToString() == "9");
       var val04 = new ui(10);
+      Debug.Assert(val04.ToString() == "10");
       var val05 = new ui(12345);
+      Debug.Assert(val05.ToString() == "12345");
       var val06 = new ui(1234567890);
+      Debug.Assert(val06.ToString() == "1234567890");
       var val07 = new ui(12345678901234567890L);
+      Debug.Assert(val07.ToString() == "12345678901234567890");
       var val08 = new ui(sbyte.MaxValue);
+      Debug.Assert(val08.ToString() == sbyte.MaxValue.ToString());
       var val09 = new ui(byte.MaxValue);
+      Debug.Assert(val09.ToString() == byte.MaxValue.ToString());
       var val10 = new ui(short.MaxValue);
+      Debug.Assert(val10.ToString() == short.MaxValue.ToString());
       var val11 = new ui(ushort.MaxValue);
+      Debug.Assert(val11.ToString() == ushort.MaxValue.ToString());
       var val12 = new ui(int.MaxValue);
+      Debug.Assert(val12.ToString() == int.MaxValue.ToString());
       var val13 = new ui(uint.MaxValue);
+      Debug.Assert(val13.ToString() == uint.MaxValue.ToString());
       var val14 = new ui(long.MaxValue);
+      Debug.Assert(val14.ToString() == long.MaxValue.ToString());
       var val15 = new ui(ulong.MaxValue);
-      //var val16 = ui.Parse("0");
-      //var val17 = ui.Parse("1");
-      //var val18 = ui.Parse("100");
-      //var val19 = ui.Parse("12345678901234567890");
-      //var val20 = ui.Parse(new string('9', 1000000));
+      Debug.Assert(val15.ToString() == ulong.MaxValue.ToString());
+      var val16 = ui.Parse("0");
+      Debug.Assert(val16.ToString() == "0");
+      var val17 = ui.Parse("1");
+      Debug.Assert(val17.ToString() == "1");
+      var val18 = ui.Parse("100");
+      Debug.Assert(val18.ToString() == "100");
+      var val19 = ui.Parse("12345678901234567890");
+      Debug.Assert(val19.ToString() == "12345678901234567890");
+      var val20 = ui.Parse(new string('9', 1000000));
+      Debug.Assert(val20.ToString() == new string('9', 1000000));
     }
 
     static void Add()
     {
-      //var val01 = new ui(123);
-      //var val02 = new ui(456);
-      //var val03 = val01 + val02; // 579
-      //var val04 = new ui(99);
-      //var val05 = val01 + val04; // 222
-      //var val06 = val04 + val02; // 555
-      //var val07 = val01 + ui.Zero; // 123
-      //var val08 = val01 + ui.One;  // 124
-      //var val09 = val01 + ui.Two;  // 125
-      //var val10 = new ui(49999);
-      //var val11 = new ui(50005);
-      //var val12 = val10 + val10; // 99998
-      //var val13 = val10 + val11; // 100004
-      //var val14 = val11 + val11; // 100010
-      //var val15 = new ui(999999999);
-      //var val16 = ui.One + val15; // 1000000000
-      //var val17 = val15 + ui.One; // 1000000000
+      var val01 = new ui(123);
+      Debug.Assert(val01.ToString() == "123");
+      var val02 = new ui(456);
+      Debug.Assert(val02.ToString() == "456");
+      var val03 = val01 + val02;
+      Debug.Assert(val03.ToString() == "579");
+      var val04 = new ui(99);
+      Debug.Assert(val04.ToString() == "99");
+      var val05 = val01 + val04;
+      Debug.Assert(val05.ToString() == "222");
+      var val06 = val04 + val02;
+      Debug.Assert(val06.ToString() == "555");
+      var val07 = val01 + ui.Zero;
+      Debug.Assert(val07.ToString() == "123");
+      var val08 = val01 + ui.One;
+      Debug.Assert(val08.ToString() == "124");
+      var val09 = val01 + ui.Two;
+      Debug.Assert(val09.ToString() == "125");
+      var val10 = new ui(49999);
+      Debug.Assert(val10.ToString() == "49999");
+      var val11 = new ui(50005);
+      Debug.Assert(val11.ToString() == "50005");
+      var val12 = val10 + val10; // 99998
+      Debug.Assert(val12.ToString() == "99998");
+      var val13 = val10 + val11; // 100004
+      Debug.Assert(val13.ToString() == "100004");
+      var val14 = val11 + val11; // 100010
+      Debug.Assert(val14.ToString() == "100010");
+      var val15 = new ui(999999999);
+      Debug.Assert(val15.ToString() == "999999999");
+      var val16 = ui.One + val15; // 1000000000
+      Debug.Assert(val16.ToString() == "1000000000");
+      var val17 = val15 + ui.One; // 1000000000
+      Debug.Assert(val17.ToString() == "1000000000");
 
-      //var val18 = ui.One;
-      //for (int i = 0; i < 8; i++) val18 += val18;
-      //// val18 == 256
-      //for (int i = 8; i < 16; i++) val18 += val18;
-      //// val18 == 65536
-      //for (int i = 16; i < 32; i++) val18 += val18;
-      //// val18 == 4294967296
-      //for (int i = 32; i < 64; i++) val18 += val18;
-      //// val18 == 18446744073709551616
-      //for (int i = 64; i < 128; i++) val18 += val18;
-      //// val18 == 340282366920938463463374607431768211456
-      //Debug.Assert(val18.ToString() == "340282366920938463463374607431768211456");
+      var val18 = ui.One;
+      for (int i = 0; i < 8; i++) val18 += val18;
+      Debug.Assert(val18.ToString() == "256");
+      for (int i = 8; i < 16; i++) val18 += val18;
+      Debug.Assert(val18.ToString() == "65536");
+      for (int i = 16; i < 32; i++) val18 += val18;
+      Debug.Assert(val18.ToString() == "4294967296");
+      for (int i = 32; i < 64; i++) val18 += val18;
+      Debug.Assert(val18.ToString() == "18446744073709551616");
+      for (int i = 64; i < 128; i++) val18 += val18;
+      Debug.Assert(val18.ToString() == "340282366920938463463374607431768211456");
 
-      //val04++; // 100
-      //val04++; // 101
-      //val15++; // 1000000000
+      Debug.Assert(val04.ToString() == "99");
+      val04++; // 100
+      Debug.Assert(val04.ToString() == "100");
+      val04++; // 101
+      Debug.Assert(val04.ToString() == "101");
+
+      Debug.Assert(val15.ToString() == "999999999");
+      val15++; // 1000000000
+      Debug.Assert(val15.ToString() == "1000000000");
     }
 
     static void Main(string[] args)
