cb3d7b2aa526810f8e98e6ce5c76cc5543084ffe
MaxKlaxxMiner <max@tetrisk.de>
2017-09-11 17:07:39 +0200

    UIntLimbs: vorbereitet

---
+++ TaschenRechnerLib/UIntLimbs/UIntLimbs.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace TaschenRechnerLib
+{
+  public sealed partial class UIntLimbs
+  {
+    /// <summary>
+    /// merkt sich die eigentlichen Zahlen-Werte (0-999999999)
+    /// </summary>
+    readonly int[] limbs;
+
+    /// <summary>
+    /// maximale Limb-Größe
+    /// </summary>
+    const int LimbMax = 1000000000;
+
+    /// <summary>
+    /// Konstruktor mit einem signierten 8-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntLimbs(sbyte val) : this((long)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem signierten 16-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntLimbs(short val) : this((long)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem signierten 32-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntLimbs(int val) : this((uint)val) { if (val < 0) throw new ArgumentOutOfRangeException("val"); }
+
+    /// <summary>
+    /// Konstruktor mit einem signierten 64-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntLimbs(long val) : this((ulong)val) { if (val < 0) throw new ArgumentOutOfRangeException("val"); }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 8-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntLimbs(byte val) : this((ulong)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 16-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntLimbs(ushort val) : this((ulong)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 32-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntLimbs(uint val) : this((ulong)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 64-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntLimbs(ulong val)
+    {
+      if (val < LimbMax) // ein-limb Wert
+      {
+        limbs = new[] { (int)val };
+      }
+      else if (val < LimbMax * (ulong)LimbMax) // zwei-limbs Wert
+      {
+        limbs = new[] { (int)(val % LimbMax), (int)(val / LimbMax) };
+      }
+      else // drei-limbs Wert
+      {
+        limbs = new[] { (int)(val % LimbMax), (int)(val / LimbMax % LimbMax), (int)(val / LimbMax / LimbMax) };
+      }
+    }
+
+    ///// <summary>
+    ///// Konstruktor mit einer Zeichenkette, welche aus einer unsignierten Integer-Zahl besteht
+    ///// </summary>
+    ///// <param name="val">Wert, welcher verwendet werden soll</param>
+    //public UIntLimbs(string val) : this(ParseInternal(val)) { }
+
+    /// <summary>
+    /// direkter Konstruktor mit den einzelnen Zahlen
+    /// </summary>
+    /// <param name="limbs">Zahlen-Array, welches direkt verwendet werden soll</param>
+    UIntLimbs(int[] limbs)
+    {
+      this.limbs = limbs;
+    }
+  }
+}
--- TaschenRechnerTest/Program.cs
+++ TaschenRechnerTest/Program.cs
@@ -3,7 +3,8 @@
 // ReSharper disable UnusedVariable
 // ReSharper disable UnusedMember.Local
 
-using ui = TaschenRechnerLib.UIntSimple;
+//using ui = TaschenRechnerLib.UIntSimple;
+using ui = TaschenRechnerLib.UIntLimbs;
 
 namespace TaschenRechnerTest
 {
@@ -26,54 +27,54 @@ namespace TaschenRechnerTest
       var val13 = new ui(uint.MaxValue);
       var val14 = new ui(long.MaxValue);
       var val15 = new ui(ulong.MaxValue);
-      var val16 = ui.Parse("0");
-      var val17 = ui.Parse("1");
-      var val18 = ui.Parse("100");
-      var val19 = ui.Parse("12345678901234567890");
-      var val20 = ui.Parse(new string('9', 1000000));
+      //var val16 = ui.Parse("0");
+      //var val17 = ui.Parse("1");
+      //var val18 = ui.Parse("100");
+      //var val19 = ui.Parse("12345678901234567890");
+      //var val20 = ui.Parse(new string('9', 1000000));
     }
 
     static void Add()
     {
-      var val01 = new ui(123);
-      var val02 = new ui(456);
-      var val03 = val01 + val02; // 579
-      var val04 = new ui(99);
-      var val05 = val01 + val04; // 222
-      var val06 = val04 + val02; // 555
-      var val07 = val01 + ui.Zero; // 123
-      var val08 = val01 + ui.One;  // 124
-      var val09 = val01 + ui.Two;  // 125
-      var val10 = new ui(49999);
-      var val11 = new ui(50005);
-      var val12 = val10 + val10; // 99998
-      var val13 = val10 + val11; // 100004
-      var val14 = val11 + val11; // 100010
-      var val15 = new ui(999999999);
-      var val16 = ui.One + val15; // 1000000000
-      var val17 = val15 + ui.One; // 1000000000
+      //var val01 = new ui(123);
+      //var val02 = new ui(456);
+      //var val03 = val01 + val02; // 579
+      //var val04 = new ui(99);
+      //var val05 = val01 + val04; // 222
+      //var val06 = val04 + val02; // 555
+      //var val07 = val01 + ui.Zero; // 123
+      //var val08 = val01 + ui.One;  // 124
+      //var val09 = val01 + ui.Two;  // 125
+      //var val10 = new ui(49999);
+      //var val11 = new ui(50005);
+      //var val12 = val10 + val10; // 99998
+      //var val13 = val10 + val11; // 100004
+      //var val14 = val11 + val11; // 100010
+      //var val15 = new ui(999999999);
+      //var val16 = ui.One + val15; // 1000000000
+      //var val17 = val15 + ui.One; // 1000000000
 
-      var val18 = ui.One;
-      for (int i = 0; i < 8; i++) val18 += val18;
-      // val18 == 256
-      for (int i = 8; i < 16; i++) val18 += val18;
-      // val18 == 65536
-      for (int i = 16; i < 32; i++) val18 += val18;
-      // val18 == 4294967296
-      for (int i = 32; i < 64; i++) val18 += val18;
-      // val18 == 18446744073709551616
-      for (int i = 64; i < 128; i++) val18 += val18;
-      // val18 == 340282366920938463463374607431768211456
-      Debug.Assert(val18.ToString() == "340282366920938463463374607431768211456");
+      //var val18 = ui.One;
+      //for (int i = 0; i < 8; i++) val18 += val18;
+      //// val18 == 256
+      //for (int i = 8; i < 16; i++) val18 += val18;
+      //// val18 == 65536
+      //for (int i = 16; i < 32; i++) val18 += val18;
+      //// val18 == 4294967296
+      //for (int i = 32; i < 64; i++) val18 += val18;
+      //// val18 == 18446744073709551616
+      //for (int i = 64; i < 128; i++) val18 += val18;
+      //// val18 == 340282366920938463463374607431768211456
+      //Debug.Assert(val18.ToString() == "340282366920938463463374607431768211456");
 
-      val04++; // 100
-      val04++; // 101
-      val15++; // 1000000000
+      //val04++; // 100
+      //val04++; // 101
+      //val15++; // 1000000000
     }
 
     static void Main(string[] args)
     {
-      // Constructor();
+      Constructor();
       Add();
     }
   }
