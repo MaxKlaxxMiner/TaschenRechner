95d898c0eef863ccc3f957b8e714425c861576c5
MaxKlaxxMiner <max@tetrisk.de>
2017-09-11 14:33:17 +0200

    Uint-Basis Klasse vorbereitet mit Konstruktoren

---
+++ TaschenRechnerLib/UintSimple/UIntSimple.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TaschenRechnerLib
+{
+  public partial class UIntSimple
+  {
+    /// <summary>
+    /// Konstruktor mit einem signierten 8-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntSimple(sbyte val) : this((long)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem signierten 16-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntSimple(short val) : this((long)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem signierten 32-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntSimple(int val) : this((uint)val) { if (val < 0) throw new ArgumentOutOfRangeException("val"); }
+
+    /// <summary>
+    /// Konstruktor mit einem signierten 64-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll (darf nicht kleiner als 0 sein)</param>
+    public UIntSimple(long val) : this((ulong)val) { if (val < 0) throw new ArgumentOutOfRangeException("val"); }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 8-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntSimple(byte val) : this((ulong)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 16-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntSimple(ushort val) : this((ulong)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 32-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntSimple(uint val) : this((ulong)val) { }
+
+    /// <summary>
+    /// Konstruktor mit einem unsignierten 64-Bit Wert
+    /// </summary>
+    /// <param name="val">Wert, welcher verwendet werden soll</param>
+    public UIntSimple(ulong val)
+    {
+
+    }
+  }
+}
