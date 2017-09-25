2426f6a663fad13beacbe327b0da2857d064ed03
MaxKlaxxMiner <max@tetrisk.de>
2017-09-25 22:33:14 +0200

    +branchless min/max

--- TaschenRechnerTest/SpeedCalc.cs
+++ TaschenRechnerTest/SpeedCalc.cs
@@ -170,13 +170,158 @@ namespace TaschenRechnerTest
       }
     }
 
+    static uint MinBranched(uint val1, uint val2)
+    {
+      return val1 < val2 ? val1 : val2;
+    }
+    static void SpeedCalcMinBranched(uint val1, uint val2)
+    {
+      const int RetryCount = 10;
+      const int TestCount = 100000000;
+
+      Console.WriteLine();
+      Console.WriteLine("  - " + val1.ToString("x").Substring(0, 1) + "," + val2.ToString("x").Substring(0, 1) + " - min - branched - ternary -");
+      Console.WriteLine();
+      for (int r = 0; r < RetryCount; r++)
+      {
+        uint l = 0;
+        var m = Stopwatch.StartNew();
+        for (int i = 0; i < TestCount; i++)
+        {
+          l += MinBranched(val1, val2);
+        }
+        m.Stop();
+        Console.WriteLine("    " + (l == Math.Min(val1, val2) * TestCount) + ": " + (m.ElapsedTicks * 1000 / (double)Stopwatch.Frequency).ToString("N2") + " ms");
+      }
+    }
+
+    static uint MaxBranched(uint val1, uint val2)
+    {
+      return val1 < val2 ? val2 : val1;
+    }
+    static void SpeedCalcMaxBranched(uint val1, uint val2)
+    {
+      const int RetryCount = 10;
+      const int TestCount = 100000000;
+
+      Console.WriteLine();
+      Console.WriteLine("  - " + val1.ToString("x").Substring(0, 1) + "," + val2.ToString("x").Substring(0, 1) + " - max - branched - ternary -");
+      Console.WriteLine();
+      for (int r = 0; r < RetryCount; r++)
+      {
+        uint l = 0;
+        var m = Stopwatch.StartNew();
+        for (int i = 0; i < TestCount; i++)
+        {
+          l += MaxBranched(val1, val2);
+        }
+        m.Stop();
+        Console.WriteLine("    " + (l == Math.Max(val1, val2) * TestCount) + ": " + (m.ElapsedTicks * 1000 / (double)Stopwatch.Frequency).ToString("N2") + " ms");
+      }
+    }
+
+    static long MinBranchless(long x, long y)
+    {
+      return x + ((y - x) >> 63 & (y - x));
+    }
+    static void SpeedCalcMinBranchless(long val1, long val2)
+    {
+      const int RetryCount = 10;
+      const int TestCount = 100000000;
+
+      Console.WriteLine();
+      Console.WriteLine("  - " + val1.ToString("x").Substring(0, 1) + "," + val2.ToString("x").Substring(0, 1) + " - min - branchless -");
+      Console.WriteLine();
+      for (int r = 0; r < RetryCount; r++)
+      {
+        long l = 0;
+        var m = Stopwatch.StartNew();
+        for (int i = 0; i < TestCount; i++)
+        {
+          l += MinBranchless(val1, val2);
+        }
+        m.Stop();
+        Console.WriteLine("    " + (l == Math.Min(val1, val2) * TestCount) + ": " + (m.ElapsedTicks * 1000 / (double)Stopwatch.Frequency).ToString("N2") + " ms");
+      }
+    }
+
+    static long MaxBranchless(long x, long y)
+    {
+      return x - ((x - y) >> 63 & (x - y));
+    }
+    static void SpeedCalcMaxBranchless(long val1, long val2)
+    {
+      const int RetryCount = 10;
+      const int TestCount = 100000000;
+
+      Console.WriteLine();
+      Console.WriteLine("  - " + val1.ToString("x").Substring(0, 1) + "," + val2.ToString("x").Substring(0, 1) + " - max - branchless -");
+      Console.WriteLine();
+      for (int r = 0; r < RetryCount; r++)
+      {
+        long l = 0;
+        var m = Stopwatch.StartNew();
+        for (int i = 0; i < TestCount; i++)
+        {
+          l += MaxBranchless(val1, val2);
+        }
+        m.Stop();
+        Console.WriteLine("    " + (l == Math.Max(val1, val2) * TestCount) + ": " + (m.ElapsedTicks * 1000 / (double)Stopwatch.Frequency).ToString("N2") + " ms");
+      }
+    }
+
+    static void SpeedCalcMinMaxBranched()
+    {
+      //SpeedCalcMinBranched(v0, v0);
+      //SpeedCalcMinBranched(v0, v1);
+      //SpeedCalcMinBranched(v0, vf);
+      //SpeedCalcMinBranched(v1, v0);
+      //SpeedCalcMinBranched(v1, v1);
+      //SpeedCalcMinBranched(v1, vf);
+      //SpeedCalcMinBranched(vf, v0);
+      //SpeedCalcMinBranched(vf, v1);
+      //SpeedCalcMinBranched(vf, vf);
+
+      //SpeedCalcMaxBranched(v0, v0);
+      //SpeedCalcMaxBranched(v0, v1);
+      //SpeedCalcMaxBranched(v0, vf);
+      //SpeedCalcMaxBranched(v1, v0);
+      //SpeedCalcMaxBranched(v1, v1);
+      //SpeedCalcMaxBranched(v1, vf);
+      //SpeedCalcMaxBranched(vf, v0);
+      //SpeedCalcMaxBranched(vf, v1);
+      //SpeedCalcMaxBranched(vf, vf);
+
+      SpeedCalcMinBranchless(v0, v0);
+      SpeedCalcMinBranchless(v0, v1);
+      SpeedCalcMinBranchless(v0, vf);
+      SpeedCalcMinBranchless(v1, v0);
+      SpeedCalcMinBranchless(v1, v1);
+      SpeedCalcMinBranchless(v1, vf);
+      SpeedCalcMinBranchless(vf, v0);
+      SpeedCalcMinBranchless(vf, v1);
+      SpeedCalcMinBranchless(vf, vf);
+
+      SpeedCalcMaxBranchless(v0, v0);
+      SpeedCalcMaxBranchless(v0, v1);
+      SpeedCalcMaxBranchless(v0, vf);
+      SpeedCalcMaxBranchless(v1, v0);
+      SpeedCalcMaxBranchless(v1, v1);
+      SpeedCalcMaxBranchless(v1, vf);
+      SpeedCalcMaxBranchless(vf, v0);
+      SpeedCalcMaxBranchless(vf, v1);
+      SpeedCalcMaxBranchless(vf, vf);
+    }
+
     static void SpeedCalc()
     {
       Console.WriteLine();
       Console.WriteLine("  --- Test Calc ---");
 
-      SpeedCalcNotNullBranched();
-      SpeedCalcNotNullBrancheless();
+      //SpeedCalcNotNullBranched();
+      //SpeedCalcNotNullBrancheless();
+
+      SpeedCalcMinMaxBranched();
     }
   }
 }
