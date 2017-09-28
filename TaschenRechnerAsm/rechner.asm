2f9986ca209b4b5f61e282db8c492aaa9b52c879
MaxKlaxxMiner <max@tetrisk.de>
2017-09-29 00:09:04 +0200

    speed optimized: mpn_add_n

--- TaschenRechnerAsm/rechner.asm
+++ TaschenRechnerAsm/rechner.asm
@@ -225,21 +225,19 @@ UIntX_Add endp
 align 16
 mpn_add_n proc export
 
-  mov r11, rcx
-  mov rcx, r9
-
-  mov rax, rcx
-  shr rcx, 2
+  mov rax, r9
+  shr r9, 2
   and rax, 3
 
-jrcxz lt4
+  test r9, r9
+je lt4
 
   push rdi
   push rsi
 
   mov r10, [rdx]
-  mov r9, [rdx + 8]
-  dec rcx
+  mov r11, [rdx + 8]
+  dec r9
 jmp mid
 
 align 16
@@ -248,50 +246,50 @@ lt4:
   dec rax
 jnz l2
   adc r10, [r8]
-  mov [r11], r10
+  mov [rcx], r10
   adc rax, rax
 ret
 
 align 16
 l2:
   dec rax
-  mov r9, [rdx + 8]
+  mov r11, [rdx + 8]
 jnz l3
   adc r10, [r8]
-  adc r9, [r8 + 8]
-  mov [r11], r10
-  mov [r11 + 8], r9
+  adc r11, [r8 + 8]
+  mov [rcx], r10
+  mov [rcx + 8], r11
   adc rax, rax
 ret
 
 align 16
 l3:
-  mov rcx, [rdx + 16]
+  mov r9, [rdx + 16]
   adc r10, [r8]
-  adc r9, [r8 + 8]
-  adc rcx, [r8 + 16]
-  mov [r11], r10
-  mov [r11 + 8], r9
-  mov [r11 + 16], rcx
+  adc r11, [r8 + 8]
+  adc r9, [r8 + 16]
+  mov [rcx], r10
+  mov [rcx + 8], r11
+  mov [rcx + 16], r9
   setc al
 ret
 
 align 16
 top:
   adc r10, [r8]
-  adc r9, [r8 + 8]
+  adc r11, [r8 + 8]
   adc rsi, [r8 + 16]
   adc rdi, [r8 + 24]
-  mov [r11], r10
+  mov [rcx], r10
   lea rdx, [rdx + 32]
-  mov [r11 + 8], r9
-  mov [r11 + 16], rsi
-  dec rcx
-  mov [r11 + 24], rdi
+  mov [rcx + 8], r11
+  mov [rcx + 16], rsi
+  dec r9
+  mov [rcx + 24], rdi
   lea r8, [r8 + 32]
   mov r10, [rdx]
-  mov r9, [rdx + 8]
-  lea r11, [r11 + 32]
+  mov r11, [rdx + 8]
+  lea rcx, [rcx + 32]
 mid:
   mov rsi, [rdx + 16]
   mov rdi, [rdx + 24]
@@ -299,15 +297,15 @@ jnz top
 
   lea rdx, [rdx + 32]
   adc r10, [r8]
-  adc r9, [r8 + 8]
+  adc r11, [r8 + 8]
   adc rsi, [r8 + 16]
   adc rdi, [r8 + 24]
   lea r8, [r8 + 32]
-  mov [r11], r10
-  mov [r11 + 8], r9
-  mov [r11 + 16], rsi
-  mov [r11 + 24], rdi
-  lea r11, [r11 + 32]
+  mov [rcx], r10
+  mov [rcx + 8], r11
+  mov [rcx + 16], rsi
+  mov [rcx + 24], rdi
+  lea rcx, [rcx + 32]
 
   pop rsi
   pop rdi
